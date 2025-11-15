1e7a1ff8cc52f2458d722ca23d2aea99cd6027c1
smanriquezfgithub <smanriquezf@gmail.com>
2025-11-15 19:15:33 -0300

    Update UploadFilePage.tsx

--- src/pages/UploadFilePage/UploadFilePage.tsx
+++ src/pages/UploadFilePage/UploadFilePage.tsx
@@ -1,9 +1,28 @@
 import { UploadCard } from '@components'
-import { Stack } from '@mantine/core'
+import { Stack, Title } from '@mantine/core'
 
 export const UploadFilePage = () => {
   return (
-    <Stack align='center' justify='center' h='90vh'>
+    <Stack
+      align="center"
+      justify="center"
+      h="90vh"
+      gap="xl"
+      style={{ textAlign: 'center' }}
+    >
+      {/* Título grande al centro, tipo “IFC Validator” */}
+      <Title
+        order={1}
+        style={{
+          fontSize: '4rem',
+          fontWeight: 600,
+          letterSpacing: '-0.03em',
+        }}
+      >
+        IFC Validator
+      </Title>
+
+      {/* Tarjeta de carga */}
       <UploadCard />
     </Stack>
   )
